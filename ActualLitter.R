7e8d76b5903648a4064ce9b75110cb1c16e862d1
Sara Sandman <ssandman@georgetowncollege.edu>
2018-03-09 15:50:03 -0500

    new update

--- ActualLitter.R
+++ ActualLitter.R
@@ -1,4 +1,5 @@
 ### make Litter Function (helper function of reproduce())
+
 makelitter<-function(momID,dadID,actualsize){
   
 actualsize<-n
@@ -26,29 +27,3 @@ individuals <- data.frame(id= as.character(ids), sex=sex, warner =
 individuals<- rbind(individual,litter)
 }
 
-## Determine Sex Function 
-
-determineSex<- function(n){
-  sample(c("M","F"),size= n, replace = T)
-}
-determineSex(3)
-
-### getChildGenes Function 
-
-getChildGenes <- function (mg,dg){
-  parentsum<-mg+dg
-  if (parentsum ==0) {
-    return
-  }
-  if (parentsum ==4) {
-    return(0)
-  }
-  if (parentsum ==3) {
-    return(sample(c(1,2), size=1, prob = c(.5,.5)))
-  }
-  if (parentsum ==2 ) {
-    return(sample(c(0,1,2),size=1, prob=c(.25,.5,.25)))
-  }
-}
-
-getChildGenes(mg=1,dg=1)
